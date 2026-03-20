e5a359209b69414351d773912b9fb8faab85ca6f
FarhanCodes-stack <farhandshaikh81@gmail.com>
2026-03-20 07:21:38 +0530

    fix: case-sensitive import issue

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import HomePage from "./landing_page/home/homePage";
+import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
 import Login from "./landing_page/login/Login";
 import AboutPage from "./landing_page/about/AboutPage";
